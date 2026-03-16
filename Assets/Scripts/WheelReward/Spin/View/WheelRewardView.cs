c9306cfe713cc282f39a686f8818883dcf059345
Cihat <cihatcitakc@gmail.com>
2026-03-16 03:35:25 +0300

    fix: bomb image is small

--- Assets/Scripts/WheelReward/Spin/View/WheelRewardView.cs
+++ Assets/Scripts/WheelReward/Spin/View/WheelRewardView.cs
@@ -14,6 +14,21 @@ namespace WheelReward.Spin.View
 
         private readonly StringBuilder _builder = new();
 
+        private RectTransform _rewardRect;
+        private Vector2 _originalAnchorMin;
+        private Vector2 _originalAnchorMax;
+        private Vector2 _originalAnchoredPosition;
+        private Vector2 _originalSizeDelta;
+
+        private void Awake()
+        {
+            _rewardRect = rewardImage.rectTransform;
+            _originalAnchorMin = _rewardRect.anchorMin;
+            _originalAnchorMax = _rewardRect.anchorMax;
+            _originalAnchoredPosition = _rewardRect.anchoredPosition;
+            _originalSizeDelta = _rewardRect.sizeDelta;
+        }
+
         public void Setup(RewardData data, int blendedCount)
         {
             id = data.Id;
@@ -21,14 +36,33 @@ namespace WheelReward.Spin.View
 
             if (data.IsBomb)
             {
+                StretchRewardImage();
                 countText.gameObject.SetActive(false);
                 return;
             }
 
+            RestoreRewardImage();
+            countText.gameObject.SetActive(true);
             _builder.Clear();
             _builder.Append("x");
             _builder.Append(blendedCount.ToString());
             countText.text = _builder.ToString();
+        }
+
+        private void StretchRewardImage()
+        {
+            _rewardRect.anchorMin = Vector2.zero;
+            _rewardRect.anchorMax = Vector2.one;
+            _rewardRect.offsetMin = Vector2.zero;
+            _rewardRect.offsetMax = Vector2.zero;
+        }
+
+        private void RestoreRewardImage()
+        {
+            _rewardRect.anchorMin = _originalAnchorMin;
+            _rewardRect.anchorMax = _originalAnchorMax;
+            _rewardRect.anchoredPosition = _originalAnchoredPosition;
+            _rewardRect.sizeDelta = _originalSizeDelta;
         }
     }
 }
